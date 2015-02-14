f812774806eb97141c295c3f7575b19b9cab4397
bennacer860 <bennacer860@gmail.com>
2015-02-14 14:50:51 -0500

    add trefis analyst price to the graph

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,7 +4,8 @@ class StocksController < ApplicationController
   end
 
   def show
-    @stock = Stock.find(params[:id])
-    @data  = @stock.prices.map{|price|  [price.date.to_time.to_i * 1000, price.market_price] }.to_json
+    @stock       = Stock.find(params[:id])
+    @data        = @stock.prices.map{|price| [price.date.to_time.to_i * 1000, price.market_price] }.to_json
+    @trefis_data = @stock.prices.map{|price| [price.date.to_time.to_i * 1000, price.trefis_analyst_price]}.to_json
   end
 end
