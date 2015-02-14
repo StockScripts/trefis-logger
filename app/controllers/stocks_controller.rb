6435ba0ee60dadbfc344b1121ff215e8d06483f3
bennacer860 <bennacer860@gmail.com>
2015-02-14 14:39:41 -0500

    add index for all companies + add show that will show one company

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,10 @@
 class StocksController < ApplicationController
   def index
+    @stocks = Stock.all
   end
 
   def show
+    @stock = Stock.find(params[:id])
+    @data  = @stock.prices.map{|price|  [price.date.to_time.to_i * 1000, price.market_price] }.to_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'stocks/index'
 
-  get 'stocks/show'
+  get 'stocks/show/:id' => 'stocks#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
