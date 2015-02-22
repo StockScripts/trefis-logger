31adef6ed9d09d6fadbaefb43aa1628d4f0522b6
bennacer860 <bennacer860@gmail.com>
2015-02-22 11:28:11 -0500

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  #get 'stocks/index' => 'stocks#index'
-
-  #get 'stocks/show/:id' => 'stocks#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'stocks#index'
+  root 'stocks#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
